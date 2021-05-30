2b9b9db83b4f668b76636e36a050c00d16fa1427
marcosangel <marcos19951911@gmail.com>
2021-05-30 19:32:15 -0300

    adicionando função que calcula forma de pagamento

--- teste.py
+++ teste.py
@@ -1,1 +1,26 @@
-#teste
+print('='*30)
+print('OPÇÕES DE PAGAMENTO')
+print('='*30)
+preço = float(input('Valor total das compras:R$'))
+print('''FORMAS DE PAGAMENTO
+[ 1 ] À vista dinheiro/cheque, 10% desconto
+[ 2 ] À vista no cartão, 5% desconto
+[ 3 ] 2x no cartão, preço normal
+[ 4 ] 3x ou mais no cartão, 20% juros''')
+opção = int(input('Digite a opção que deseja: '))
+if opção == 1:
+    total = preço - (preço * 10 / 100)
+elif opção == 2:
+    total = preço - (preço * 5 / 100)
+elif opção == 3:
+    total = preço
+    parcela = total / 2
+    print('Sua compra será parcelada em 2x de R${:.2f}, sem juros!'.format(parcela))
+elif opção == 4:
+     total = preço + (preço * 20 / 100)
+     parc = int(input('Quantas parcelas?: '))
+     parcela = total / parc
+     print('Sua compra será parcelada em {}x de R${:.2f} com juros.'.format(parc,parcela))
+else:
+    print('Opção INVALIDA! Tenta novamente')
+print('Voce pagará o valor total de R${:.2f}'.format(total))
